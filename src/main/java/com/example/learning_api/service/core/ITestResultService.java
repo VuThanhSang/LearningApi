7b9260bdd39bf3cfe41fd38bb8e5781c2e5a7f04
unknown <zimb240@gmail.com>
2024-08-02 22:19:31 +0700

    test exit logs

--- src/main/java/com/example/learning_api/service/core/ITestResultService.java
+++ src/main/java/com/example/learning_api/service/core/ITestResultService.java
@@ -1,10 +1,13 @@
 package com.example.learning_api.service.core;
 
+import com.example.learning_api.dto.request.test.CreateExitLogRequest;
 import com.example.learning_api.dto.request.test.CreateTestResultRequest;
 import com.example.learning_api.dto.request.test.SaveProgressRequest;
 import com.example.learning_api.dto.request.test.UpdateTestResultRequest;
 import com.example.learning_api.dto.response.question.GetQuestionsResponse;
 import com.example.learning_api.dto.response.test.StartTestResponse;
+import com.example.learning_api.entity.sql.database.StudentTestExitLogEntity;
+import com.example.learning_api.repository.database.StudentTestExitLogRepository;
 
 import java.util.List;
 
@@ -13,5 +16,6 @@ public interface ITestResultService {
     void updateTestResult(UpdateTestResultRequest body);
     void deleteTestResult(String studentId, String courseId);
     void saveProgress(SaveProgressRequest body);
-
+    void exitTestLog(CreateExitLogRequest body);
+    List<StudentTestExitLogEntity> getTestResult(String studentId, String TestResultId);
 }
