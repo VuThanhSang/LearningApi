6b07b3e8ef38ac166d74371bdd7347bd75fea562
unknown <zimb240@gmail.com>
2024-08-17 22:55:26 +0700

    filter deadline

--- src/main/java/com/example/learning_api/entity/sql/database/DeadlineSubmissionsEntity.java
+++ src/main/java/com/example/learning_api/entity/sql/database/DeadlineSubmissionsEntity.java
@@ -5,6 +5,7 @@ import com.example.learning_api.enums.DeadlineSubmissionStatus;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.File;
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Document(collection = "deadline_submissions")
+@CompoundIndex(def = "{'title': 'text', 'submission': 'text'}")
 public class DeadlineSubmissionsEntity {
     @Id
     private String id;
