b557c1621a37695338e5ebe562cdadb15280f80e
unknown <zimb240@gmail.com>
2024-12-09 23:55:48 +0700

    fix api

---
+++ src/main/java/com/example/learning_api/dto/response/feedback/GetFeedBackByStudentResponse.java
@@ -0,0 +1,9 @@
+package com.example.learning_api.dto.response.feedback;
+
+import com.example.learning_api.entity.sql.database.FeedbackEntity;
+
+import java.util.List;
+
+public class GetFeedBackByStudentResponse {
+    private List<FeedbackEntity> feedbacks;
+}
